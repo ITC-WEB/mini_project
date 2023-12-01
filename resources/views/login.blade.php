aa0cc6b5d96d729f6affd61f259b1934ac756915
kharisma tara mukti <kharismataram@gmail.com>
2023-12-01 17:53:02 +0700

    login dan register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,28 +17,32 @@
     <div class="container-fluid h-custom">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-md-9 col-lg-6 col-xl-5">
-          <img src="frontend/images/loginsy.png" class="img-fluid" alt="Sample image" />
+          <img src="frontend/images/login2.png" class="img-fluid" alt="Sample image" />
         </div>
         <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
           <form action="" method="post">
             @csrf
-            <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start selamat mt-5">SELAMAT DATANG di INTERMEDIA TRANS</div>
-            <p class="fw-bold mt-2 pt-1 mb-0 sudahakun">sudah memiliki akun? <a href="/register" class="link-masuk text-decoration-none">masuk</a></p>
+            <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start selamat mt-5">Welcome to</div>
+            <div class="textlog d-flex flex-row align-items-center justify-content-center justify-content-lg-start selamat mb-4">Intermedia Trans !</div>
             <!-- Email input -->
             <div class="form-outline mb-4">
-              <label class="form-label" for="form3Example3">Email address</label>
-              <input name="email" type="email" id="form3Example3" class="form-control form-control-lg" placeholder="Enter a valid email address" required />
+            <input name="email" type="email" id="form3Example3" class="form-control form-control-lg custom-input" placeholder="email address" required />
             </div>
 
             <!-- Password input -->
             <div class="form-outline mb-3">
-              <label class="form-label" for="form3Example4">Password</label>
-              <input name="password" type="password" id="form3Example4" class="form-control form-control-lg" placeholder="Enter password" required />
+              <input name="password" type="password" id="form3Example4" class="form-control form-control-lg custom-input" placeholder="Password" required />
             </div>
+            <div class="form-group form-check mt-2">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Remember Me ! <a href="#" class="link-masuk2 ml-5 ">forgot password</a></label>
+                      </div>
 
-            <div class="text-center text-lg-start mt-4 pt-2">
-              <button type="submit" class=" btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem">Login</button>
+            <div class=" text-lg-start mt-4 pt-2 ">
+              <button type="submit" class="butonlog btn btn-lg" style="padding-left: 2rem; padding-right: 2rem">Login</button>
             </div>
+            <p class=mt-4>
+              Dont have an account ? <a href="#" class="link-masuk ">sign up</a></p>
           </form>
         </div>
       </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
@@ -16,86 +16,70 @@
 
     <!-- Style -->
     <link rel="stylesheet" href="{{asset('login/css/style.css')}}">
+    <link rel=" stylesheet" href="{{asset('frontend/styles/main.css')}}" />
 
     <title>Register ITC Trans</title>
 </head>
 
 <body>
-    <section style="height: 100vh;">
-        <div class="container py-5">
-            <div class="row d-flex justify-content-start align-items-center h-100">
-                <div class="col-12">
-                    <div class="row g-0">
-                        <div class="col-xl-6 d-none d-xl-block">
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img4.webp" alt="Sample photo" class="img-fluid">
-                        </div>
-                        <div class="col-md-6">
-                            <div class="card-body p-md-5 text-black">
-                                <h3 class="mb-5 text-uppercase">Registration Itc-Trans</h3>
-                                <form action="" method="post" enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="form-group last mb-3">
-                                        <label for="name">Full Name</label>
-                                        <input type="text" name="name" class="form-control" placeholder="Your Name" id="name">
-                                    </div>
-                                    <div class="form-group last mb-3">
-                                        <label for="email">Email</label>
-                                        <input type="email" name="email" class="form-control" placeholder="Your Email" id="email">
-                                    </div>
-                                    <div class="form-group last mb-3">
-                                        <label for="password">Password</label>
-                                        <input type="password" name="password" class="form-control" placeholder="Your Password" id="password">
-                                    </div>
-                                    <div class="form-group last mb-3">
-                                        <label for="phone">Phone</label>
-                                        <input type="number" name="phone" class="form-control">
-                                    </div>
+   <section>
+    <div class="container">
+        <form action="">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 col-sm-12">
+            <h3 class="mb-2 mt-5 registra">Registration to</h3>
+            <h3 class="mb-5 registra2">Intermedia Trans</h3>
+            <div class="form-group last mb-5">
+                <input type="text" name="name" class="form-control custom-input" placeholder="Nama" id="name">
+                </div>
+            <div class="form-group last mb-5">
+                <input type="email" name="email" class="form-control custom-input" placeholder="Alamat Email" id="email">
+                 </div>
+            <div class="form-group last mb-5">
+                <input type="password" name="password" class="form-control custom-input" placeholder="Kata Sandi" id="password">
+                </div>
+            <div class="form-group last mb-5">
+                <input type="number" name="phone" class="form-control custom-input " placeholder="Nomor Hp/WhatsApp" id="phone">
+                </div>
 
-                                    <div class="form-group last mb-3">
-                                        <label for="alamat">Address</label>
-                                        <input type="text" name="alamat" class="form-control">
-                                    </div>
+            </div>
+            <div class="col-lg-6 col-md-12 col-sm-12">
+            <div class="form-group last mt-5">
+                <input type="text" name="alamat" class="form-control custom-input" placeholder="Alamat" id="alamat">
+                </div>
+                <div class=" mb-5 py-2">
+                    <h6 class="mb-0 me-4">Jenis Kelamin </h6>
+                    <div class="gender form-check form-check-inline mb-0 me-4 mt-5">
+                        <input class="form-check-input" type="radio" name="gender" id="maleGender" value="male" />
+                        <label class="form-check-label" name="gender" for="male">laki-laki</label>
+                    </div>
+                    <div class="gender form-check form-check-inline mb-0 me-4 mt-5">
+                        <input class="form-check-input" type="radio" name="gender" id="femaleGender" value="female" />
+                        <label class="form-check-label" name="gender" for="female">perempuan</label>
+                    </div>
 
-                                    <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
-
-                                        <h6 class="mb-0 me-4">Gender: </h6>
-                                        <div class="form-check form-check-inline mb-0 me-4">
-                                            <input class="form-check-input" type="radio" name="gender" id="maleGender" value="male" />
-                                            <label class="form-check-label" name="gender" for="male">Male</label>
-                                        </div>
-                                        <div class="form-check form-check-inline mb-0 me-4">
-                                            <input class="form-check-input" type="radio" name="gender" id="femaleGender" value="female" />
-                                            <label class="form-check-label" name="gender" for="female">Female</label>
-                                        </div>
-
-                                    </div>
-                                    <div class="row">
-                                        <div class="col md-6 mb-4">
-                                            <label for="data_id">KTP</label>
-                                            <input type="file" name="ktp">
-                                        </div>
-
-                                    </div>
-                                    <div class="row">
-                                        <div class="col md-6 mb-4">
-                                            <label for="data_id">SIM</label>
-                                            <input type="file" name="sim">
-                                        </div>
-                                    </div>
-                                    <span class="ml-auto"><a href="/" class="forgot-pass">Back To Home</a></span>
-                                    <input type="submit" class="btn btn-block btn-primary">
-                                </form>
-                            </div>
-
-
+                    <div class="row">
+                        <div class="col md-6 mt-5">
+                        <label for="data_id">Unggah Kartu Tanda Penduduk (KTP)</label>
+                        <input class="mt-3" type="file" name="ktp">
                         </div>
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="col md-6 mb-4">
+                        <label class="mt-4" for="data_id">Unggah Surat Izin Mengemudi (SIM - A)</label>
+                        <input class="mt-3" type="file" name="sim">
+                         </div>
+                    </div>
+                    <a href="/" class="forgot-pass"></a>
+                                <input type="submit" class="btn butonlog btn-block btn-primary">
+
+                  <p class="have-account mt-4" > have an account ? <a href="#" class="link-masuk ">login</a></p>
+                               
             </div>
-        </div>
-        </div>
-        </div>
-    </section>
+        </form>
+    </div>
+    </div>
+   </section>
     <script src="{{asset('login/js/jquery-3.3.1.min.js')}}"></script>
     <script src="{{asset('login/js/popper.min.js')}}"></script>
     <script src="{{asset('login/js/bootstrap.min.js')}}"></script>
