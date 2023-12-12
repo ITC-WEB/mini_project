3c130fa1b7e9542e9a0917c7b7f8b3584acbfee8
kharismatara <kharismataram@gmail.com>
2023-12-12 13:53:11 +0700

    aos tentang kami

--- resources/views/frontend/pages/tentangkami.blade.php
+++ resources/views/frontend/pages/tentangkami.blade.php
@@ -8,15 +8,11 @@ TentangKami
 <header class="text-left headertentangkami">
     <h1 class="ml-5">
         Tentang <br /> Intermedia Trans
-
-
     </h1>
     <p class="mt-3 ml-5">
         Kenali perusahaan kami lebih dekat
     </p>
-
 </header>
-<!-- Hero -->
 <!-- Hero -->
 <!--Tentang Kami -->
 <section class="tentangkami mb-5">
@@ -24,12 +20,12 @@ TentangKami
         <div class="text-center">
             <div class="container px-3 mt-5">
                 <div class="row mb-md-0 mb-5 justify-content-between">
-                    <div class="col-lg-6 py-lg-5">
+                    <div class="col-lg-6 py-lg-5" data-aos="fade-right">>
                         <div class="text-center text-lg-left mt-5">
                             <img src="frontend/images/contententangkami.png" class="tentangkami-img img-fluid float-end" />
                         </div>
                     </div>
-                    <div class="col-lg-6 py-lg-5 ">
+                    <div class="col-lg-6 py-lg-5" data-aos="fade-left">>
                         <div class="tentangkami-content py-5">
                             <h1 class="text-lg-left font-weight-bold tentangkamiconten1">Intermedia Trans</h1>
                             <p class="text-lg-left mt-lg-2 tentangkami-conten pararaf-tentangkami">
@@ -51,26 +47,28 @@ TentangKami
 <section class="berproses mt-5">
     <div class="berproses-view" id="berproses">
         <div class="text-center p-5">
-            <h4 class="berposes1">Proses Kami Berkerja </h4>
+            <div class="" data-aos="fade-down">
+                <h4 class="berposes1">Proses Kami Berkerja </h4>
             <h2 class="berproses2 font-weight-bold">Bagaimana Kami Berkerja ?</h2>
             <p class="berproses3">Kami bekerja sesuai dengan prosedur yang berlaku dan menjadikan <br> permasalahan customer sebagai konsep dasar rental mobil </p>
+            </div>
             <div class="container px-3 mt-5">
                 <div class="row mb-md-0 mb-5 justify-content-around">
-                    <div class="col-lg-4 py-lg-5">
+                    <div class="col-lg-4 py-lg-5"  data-aos="zoom-in-right">
                         <div class="text-center text-lg-center mt-3">
                             <img src="frontend/images/Notepad.png" class="bp-img img-fluid r" style="height: 64px; width: 64px;" />
                             <h3 class="font-weight-bold">Reservasi</h3>
                             <h4>Reservasi melalui form yang tersedia dan lakukan pembayaran</h4>
                         </div>
                     </div>
-                    <div class="col-lg-4 py-lg-5">
+                    <div class="col-lg-4 py-lg-5" data-aos="fade-left">
                         <div class="text-center text-lg-center mt-3">
                             <img src="frontend/images/verifikasi.png" class="bp-img img-fluid " style="height: 64px; width: 64px;" />
                             <h3 class="font-weight-bold">Verifikasi</h3>
                             <h4>Anda akan mendapatkan notifikasi melalui email untuk persetujuan reservasi</h4>
                         </div>
                     </div>
-                    <div class="col-lg-4 py-lg-5">
+                    <div class="col-lg-4 py-lg-5"  data-aos="zoom-in-right">
                         <div class="text-center text-lg-center mt-3">
                             <img src="frontend/images/deal.png" class="bp-img img-fluid " style="height: 64px; width: 64px;" />
                             <h3 class="font-weight-bold">Deal </h3>
@@ -81,6 +79,5 @@ TentangKami
             </div>
         </div>
     </div>
-
 </section>
 @endsection
