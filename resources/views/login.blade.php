5d11f26062c0efc2384cf3a059e61dbbb02f9f58
kharismatara <kharismataram@gmail.com>
2023-12-18 08:43:44 +0700

    hh

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,6 +14,7 @@
 </head>
 
 <body>
+{{--  <div id="aa" style="background-color: #71c4d"></div>  --}}
   <section class="vh-100">
     <div class="container-fluid h-custom">
       @include('sweetalert::alert')
