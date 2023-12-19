bdd56dea827d7624e0eeac34e34c7f19073ec9d3
Fadilatul Rohmania <fadilatulfadilatul95@gmail.com>
2023-12-19 23:08:09 +0700

    pembaruan nomor pesan cust

--- app/Http/Controllers/Frontend/PembayaranController.php
+++ app/Http/Controllers/Frontend/PembayaranController.php
@@ -28,6 +28,6 @@ class PembayaranController extends Controller
         $bukti_id = $bukti_bayar->id;
         $peminjaman = Peminjaman::where('user_id', (int)$request->user_id)->latest('id')->first();
         $peminjaman->update(['bukti_id' => $bukti_id]);
-        return redirect("https://wa.me/08989392968");
+        return redirect("https://wa.wizard.id/b104aa");
     }
 }
